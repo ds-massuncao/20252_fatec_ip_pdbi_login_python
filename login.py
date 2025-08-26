0a83196f0faa197b49146d35251a4593a9174234
Marcos Assunção <marcos.silva455@fatec.sp.gov.br>
2025-08-26 10:49:38 -0300

    learn(ap07): encerra apostila

---
+++ login.py
@@ -0,0 +1,44 @@
+import psycopg
+
+class Usuario:
+    def __init__(self, login, senha):
+        self.login = login
+        self.senha = senha
+
+def existe(usuario):
+    with psycopg.connect(
+        host = 'localhost',
+        port = 5432
+        dbname = '20252_fatec_ipi_pbdi_marcos_assuncao'
+        user = 'postgres'
+        password  = '1234'
+    ) as conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute(
+                'SELECT * FROM tb_usuario WHERE login = %s AND senha = %s',
+                (f'{usuario.login}', f'{usuario.senha}')
+            )
+            result = cursor.fetchone()
+            return result != None
+
+def menu():
+    texto = '0-Sair\n1-login\n2-logoff\n'
+    usuario = None
+    op = int(input(texto))
+    while op != 0:
+        if op == 1:
+            login = input('Digite seu login')
+            senha = input('Digite sua senha')
+            usuario = Usuario(login,senha)
+            print(
+                "usuario OK" if existe(usuario) else "Usuario NOK")        
+        elif op == 2:
+            usuario = None
+            print('logoff feito com sucesso')
+        op = int(input(texto))
+        
+
+    else: 
+        print('Até Mais!!')
+
+menu()
